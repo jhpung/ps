a6fb0734cf4bd06ee2c51f3ba3d8f580f205ca61
jhpung <jhpung.dev@gmail.com>
2021-08-01 23:46:14 +0900

    2042 try - 2

--- Baekjoon/gold/2042.cpp
+++ Baekjoon/gold/2042.cpp
@@ -1,66 +1,79 @@
 #include <iostream>
 #include <memory.h>
+#include <math.h>
 using namespace std;
 
-int N, M, K;
-int *nums;
-int a, b, c;
-int *tree;
+typedef long long ll;
 
-int init(int start, int end, int node) {
-    if(start == end) return tree[node] = nums[start];
+ll N, M, K;
+ll a, b, c;
+ll *tree;
+ll *nums;
+
+int init(int start, int end, ll node)
+{
+    if (start == end)
+    {
+        return tree[node] = nums[start];
+    }
     int mid = (start + end) / 2;
     return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
 }
 
-int sum(int start, int end, int node, int left, int right) {
-    if(left > end || right < start) return 0;
-    if(left <= start && end <= right) return tree[node];
+int sum(int start, int end, ll node, int left, int right)
+{
+    if (start > right || end < left)
+    {
+        return 0;
+    }
+    if (start >= left && end <= right)
+    {
+        return tree[node];
+    }
     int mid = (start + end) / 2;
-    return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right); 
+    return tree[node] = sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
 }
 
-void update(int start, int end, int node, int index, int dif) {
-    if(index < start || index > end) return;
-    tree[node] += dif;
-    if(start == end) return;
+void update(int start, int end, ll node, ll index, ll diff)
+{
+    if (index < start || index > end)
+        return;
+    tree[node] += diff;
+    if (start == end)
+    {
+        nums[index] += diff;
+        return;
+    }
+
     int mid = (start + end) / 2;
-    update(start, mid, node * 2, index, dif);
-    update(mid + 1, end, node * 2 + 1, index, dif);
+    update(start, mid, node * 2, index, diff);
+    update(mid + 1, end, node * 2 + 1, index, diff);
 }
 
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
+    ll size;
     cin >> N >> M >> K;
-    cout << N << M << K;
-    nums = (int*)malloc(sizeof(int) * N);
-    tree = (int*)malloc(sizeof(int) * N * 4);
-    memset(tree, 0, sizeof(int) * N * 4);
-    for(int i = 0; i < N; i++) {
+
+    nums = (ll *)malloc(sizeof(ll) * N);
+    size = 1 << (ll)(ceill(log2l(N) + 1));
+    tree = (ll *)malloc(sizeof(ll) * size);
+    for (int i = 0; i < N; i++)
+    {
         cin >> nums[i];
     }
     init(0, N - 1, 1);
-    for(int i = 0; i < N; i++) {
-        cout << nums[i] << ' ';
-    }
-    cout << endl;
-    for(int i = 1; i < N * 4; i++) {
-        cout << tree[i] << ' ';
-    }
-    cout << endl;
-    for(int i = 0; i < M + K; i++) {
+
+    for (int i = 0; i < M + K; i++)
+    {
         cin >> a >> b >> c;
-        switch(a) {
-            case 1:
-                update(0, N - 1, 1, b, c - nums[b]);
-                for(int i = 1; i < N * 4; i++) {
-                    cout << tree[i] << ' ';
-                }
-                cout << endl;
-            break;
-            case 2:
-                cout << sum(0, N - 1, 1, b, c) << endl;
-            break;
+        if (a == 1)
+        {
+            update(0, N - 1, 1, b - 1, c - nums[b - 1]);
+        }
+        if (a == 2)
+        {
+            cout << sum(0, N - 1, 1, b - 1, c - 1) << endl;
         }
     }
 }
